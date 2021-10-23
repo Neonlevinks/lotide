417aa43be289473ece267e26b8e772b83e0bc71c
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2021-10-23 18:10:54 +0000

    Test that empty arrays, and arrays with 1 item,  return a tail with 0 length

--- tail.js
+++ tail.js
@@ -17,3 +17,5 @@ const tail = (array) => {
 
 //TEST CODE
 assertEqual((tail(["a", "s", "d"])).length, 2); //tail should have 1 less length than the original array
+assertEqual((tail([])).length, 0); //the tail of an empty array should have a length of zero
+assertEqual((tail(["a"])).length, 0); //the tail of an empty array should have a length of zero
