a717bc174bc034d7f12971029ce1878f9d5d0f9d
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2021-10-23 17:27:40 +0000

    lint assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -11,4 +11,4 @@ const assertEqual = function(actual, expected) {
 assertEqual("Lighthouse Labs", "Bootcamp");//testing non-identical strings
 assertEqual(1, 1); //testing identical numbers
 assertEqual("Kevin", "Kevin"); //testing identical strings
-assertEqual(1, 10)//testing non-identical numbers
+assertEqual(1, 10);//testing non-identical numbers
