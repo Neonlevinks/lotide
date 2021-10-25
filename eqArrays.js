6240a51f41dcb803dc32b37c217189eec2fb7ead
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2021-10-25 00:45:26 +0000

    Tested code to make sure function works

--- eqArrays.js
+++ eqArrays.js
@@ -7,5 +7,21 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = (array1, array2) => {
-  array1.join() === array2.join() ? true : false ;
+  let result = false;
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] === array2[i]) {
+        result = true;
+      } else if (array1[i] !== array2[i]) {
+        result = false;
+        break;
+      }
+    }
+    return result;
+  }
+  return result;  
 };
+
+
+//TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
