100adc61d9a3f53eaef81841cce0256097146c87
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2021-10-27 00:47:56 +0000

    Lint middle.js

--- middle.js
+++ middle.js
@@ -1,7 +1,7 @@
 const eqArrays = (array1, array2) => {
   let result = false;
   if (array1.length === 0 && array2.length === 0) {
-    result = true
+    result = true;
   } else if (array1.length === array2.length) {
     for (let i = 0; i < array1.length; i++) {
       if (array1[i] === array2[i]) {
@@ -13,7 +13,7 @@ const eqArrays = (array1, array2) => {
     }
   }
   return result;
-}
+};
   
 
 
@@ -29,23 +29,20 @@ const middle = (array) => {
   let result = [];
   if (array.length > 2) {
     if (array.length % 2 === 0) {//if the length is even
-      let midHigher = array.length/2;
-      let midLower = array.length/2 - 1;
+      let midHigher = array.length / 2;
+      let midLower = array.length / 2 - 1;
       result.push(array[midLower]);
       result.push(array[midHigher]);
     } else if (array.length % 2 !== 0) {//if the length is odd
-      let mid = Math.floor(array.length/2);
+      let mid = Math.floor(array.length / 2);
       result.push(array[mid]);
-      }
-    } 
+    }
+  }
   return result;
-}
+};
 
 //TEST CODE
 assertArraysEqual(middle([1, 2, 3]),[2]);
-
-
-
 assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
 assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
