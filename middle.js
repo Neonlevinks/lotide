a2bb6e50700f045405da0c278adda13e95b2c363
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2021-10-27 00:24:15 +0000

    Implemented working function to find the middle of arrays

--- middle.js
+++ middle.js
@@ -24,14 +24,18 @@ const assertArraysEqual = (array1, array2) => {
 
 const middle = (array) => {
   let result = [];
-  if (array.length <= 2) {
+  if (array.length > 2) {
     if (array.length % 2 === 0) {//if the length is even
-      result.push(mid)
+      let midHigher = array.length/2;
+      let midLower = array.length/2 - 1;
+      result.push(array[midLower]);
+      result.push(array[midHigher]);
     } else if (array.length % 2 !== 0) {//if the length is odd
-      result.push(mid)
-    }
-    
-  }
+      let mid = Math.floor(array.length/2);
+      result.push(array[mid]);
+      }
+    } 
+  return console.log(result);
 }
 
 //TEST CODE
