a5cc9bba3c0c1f1ca55b67fb8d4b212817afcc0d
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2021-10-27 00:48:30 +0000

    Update eqArrays.js to handle empty arrays

--- eqArrays.js
+++ eqArrays.js
@@ -8,7 +8,9 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = (array1, array2) => {
   let result = false;
-  if (array1.length === array2.length) {
+  if (array1.length === 0 && array2.length === 0) {
+    result = true;
+  } else if (array1.length === array2.length) {
     for (let i = 0; i < array1.length; i++) {
       if (array1[i] === array2[i]) {
         result = true;
@@ -17,9 +19,8 @@ const eqArrays = (array1, array2) => {
         break;
       }
     }
-    return result;
   }
-  return result;  
+  return result;
 };
 
 
