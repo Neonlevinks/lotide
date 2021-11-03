934371cde1f9daaacfe855bbd667950ee2616066
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2021-11-03 00:28:46 +0000

    Implement countLetters function

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+//assertEqual function copied
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = (inputString) => {
+  results = {};
+
+  for (const letter of inputString) {
+    if (results[letter]) {
+        results[letter] += 1;
+    } else if (letter === " ") {
+    
+    } else {
+        results[letter] = 1;
+    }
+    
+  }
+  console.log(results)
+  return results;
+}
+
+countLetters("lighthouse in the house");
