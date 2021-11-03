c61fd4a3681b0cc0b082070836a60efdf1aceaa6
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2021-11-03 01:46:23 +0000

    Implement function and run tests

--- takeUntil.js
+++ takeUntil.js
@@ -21,3 +21,23 @@ const assertArraysEqual = (array1, array2) => {
     console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
   }
 };
+
+const takeUntil = (array, callback) => {
+  const results = [];
+  for (let index of array) {
+    if (callback(index)) {
+      break;
+    } else {
+      results.push(index);
+    }
+  }
+  return results;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ])
