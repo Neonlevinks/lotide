2153c141b84344bf744aaf4d11a44526c71ff2cb
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2021-11-03 01:39:54 +0000

    Add tests and verify they pass

--- map.js
+++ map.js
@@ -37,3 +37,7 @@ const results2 = map(words, word => word.length) //test 1 to show length of each
 const results3 = map(words, word => `${word} David Bowie`)// test 2 to show David Bowie after each word
 console.log(results1);
 const results4 = map(words, word => `${word} starts with ${word[0]}`)// shows what each word starts with
+
+assertArraysEqual(results2, [6, 7, 2, 5, 3]) 
+assertArraysEqual(results3, ["ground David Bowie","control David Bowie","to David Bowie","major David Bowie","tom David Bowie"])
+assertArraysEqual(results4, ["ground starts with g", "control starts with c", "to starts with t", "major starts with m", "tom starts with t"])
