6dc72c4385f68908bab1b6bc52fb34139d996285
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2021-11-03 00:49:48 +0000

    Add empty function and test code

--- eqObjects.js
+++ eqObjects.js
@@ -5,3 +5,14 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+const eqObjects = (object1, object2) => {
+  //put code here
+}
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual((eqObjects(ab, ba)), true);
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false);
